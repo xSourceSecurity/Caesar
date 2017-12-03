c231996a9e2c0108265f2a76881ae0420f5b4f04
justArmenia <31382328+xSourceSecurity@users.noreply.github.com>
2017-12-03 18:39:46 +0400

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -89,7 +89,7 @@ elif testingInput == "dcr":
 	result = "[+] Success decryption..\n" + Caesar.decrypt(sys.argv[2], int(sys.argv[4]))
 elif testingInput == "hlp":
 	result = '''
-	[Caesar crypter / decrypter]
+	[Caesar cipher / decypher]
 	===================================================
 	-h or --help		|	For helping
 
